85369eb617dd72df3c13d60be15f1748ef36370a
Oleg Kulik <okulik@gorillagroup.com>
2018-03-07 01:41:42 +0200

    fix travis config for db;

--- config/autoload/db.travis.php
+++ config/autoload/db.travis.php
@@ -3,6 +3,6 @@ declare(strict_types=1);
 
 return [
     'db' => [
-        'url' => 'mysql://root@mysql/dataflow-server',
+        'url' => 'mysql://root@127.0.0.1:3306/dataflow-server?charset=UTF8',
     ]
 ];
