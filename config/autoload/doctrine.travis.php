8fe3c9d1bf7e0bfd0204a67f17150f522cb7c1ee
Oleg Kulik <okulik@gorillagroup.com>
2018-03-07 14:28:36 +0200

    change doctrine proxy dir for travis;

---
+++ config/autoload/doctrine.travis.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'doctrine' => [
+        'dev_mode' => true,
+        'configuration' => [
+            'proxy_dir' => 'data/'
+        ]
+    ]
+];
